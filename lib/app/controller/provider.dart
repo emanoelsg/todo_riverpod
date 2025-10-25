9040292c5e3308cfb89ab3c9b1b92d9133893519
Emanuel Gomes <dapouco3@gmail.com>
2025-10-25 16:21:47 -0300

    firts commit

---
+++ lib/app/controller/provider.dart
@@ -0,0 +1,6 @@
+// app/controller/provider.dart
+import 'package:flutter_riverpod/legacy.dart';
+
+final number = StateProvider<int>((ref) {
+  return 0;
+});
---
+++ lib/app/view/home_page.dart
@@ -0,0 +1,37 @@
+// app/view/home_page.dart
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_riverpod/app/controller/provider.dart';
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+             Text(
+              '${ref.watch(number)}',
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                ref.watch(number.notifier).state++;
+              },
+              label: const Icon(Icons.add),
+            ),
+                ElevatedButton.icon(
+              onPressed: () {
+                ref.watch(number.notifier).state--;
+              },
+              label: const Icon(Icons.remove),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
